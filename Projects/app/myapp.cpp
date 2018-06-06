1b3c6b8513a6ee8edb3a158f5f71a4d959df8149
shamaniyak <shamaniyak@gmail.com>
2018-06-07 02:06:09 +0300

    Добавлена функция

--- Projects/app/myapp.cpp
+++ Projects/app/myapp.cpp
@@ -1,6 +1,8 @@
 #include "myapp.h"
 
+#include <QDir>
 #include <QGuiApplication>
+#include <QUrl>
 
 MyApp::MyApp(QObject *parent) : QObject(parent)
 {
@@ -10,4 +12,16 @@ MyApp::MyApp(QObject *parent) : QObject(parent)
 QString MyApp::applicationDirPath() const
 {
   return QGuiApplication::applicationDirPath();
+}
+
+QString MyApp::urlToNativeFilePath(const QString &filePath)
+{
+  auto fName = filePath;
+  QUrl url(fName);
+  if(url.isLocalFile()) {
+    fName = url.toLocalFile();
+    fName = QDir::toNativeSeparators(fName);
+    //qDebug() << fName;
+  }
+  return fName;
 }
